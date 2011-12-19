f25746e4c17fbfc13a1944cb6548518adabf26f3
David Pitt <dpitt@keyholesoftware.com>
2011-12-18 19:55:59 -0600

    Added usage/info documenation to unit test and factory class

--- src/test/java/com/khs/batch/report/ExampleTimesheetReportFactory.java
+++ src/test/java/com/khs/batch/report/ExampleTimesheetReportFactory.java
@@ -1,54 +1,42 @@
 package com.khs.batch.report;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import com.khs.batch.report.Column;
-import com.khs.batch.report.ReportFactory;
 /**
  * Example Timesheet report format (i.e. columns,headings, footings, groupings, totaling, etc..)
+ * 
  * @author dpitt
- *
  */
 public class ExampleTimesheetReportFactory extends ReportFactory {
-
 	// column ids
-
 	public final static String WEEKEND = "weekend";
 	public final static String DEPARTMENT = "department";
 	public final static String EMPLOYEE = "employee";
 	public final static String HOURS = "hours";
 
-
 	@Override
-	public String[] getHeader() {
-
-		return new String[] { "Example Report",
-				"~Example Timesheet Report~Page #" };
+	// Report Header List
+	// Each element in list is a header line
+	// ~ is used to justify header text LEFT,CENTER,RIGHT
+	public List<String> getHeader() {
+		return Arrays.asList("Example Report", "~Example Timesheet Report~Page #");
 	}
 
 	@Override
+	// Report Columns Column definitions options...
+	// Column.NewGroup(<col id>,<col value>)
+	// Column.NewTotal(<col id>,<col value>)
+	// Column.NewNumeric(<col id>,<col value>)
 	public List<Column> getColumns() {
-
-		List<Column> cols = new ArrayList<Column>();
-		cols.add( Column.New(WEEKEND, "Weekend") );
-	
-		// group on department
-		cols.add( Column.NewGroup(DEPARTMENT,"Department") );
-		
-		cols.add( Column.New(EMPLOYEE, "Employee" ) );
-		
-		// total on hours
-		cols.add( Column.NewTotal(HOURS,"Hours") );
-
-		return cols;
+		return Arrays.asList(Column.New(WEEKEND, "Weekend"), Column.NewGroup(DEPARTMENT, "Department"), Column.New(EMPLOYEE, "Employee"), Column.NewTotal(HOURS, "Hours"));
 	}
 
 	@Override
-	public String[] getFooter() {
-
-		return new String[] { "~Footer for " };
-
+	// Report Footer List
+	// Each element in list is a header line
+	// ~ is used to justify header text LEFT,CENTER,RIGHT
+	public List<String> getFooter() {
+		return Arrays.asList("~Footer for ");
 	}
-
 }
--- src/test/java/com/khs/batch/report/ReportTest.java
+++ src/test/java/com/khs/batch/report/ReportTest.java
@@ -9,23 +9,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(locations={"/launch-context.xml"})
+@ContextConfiguration(locations = { "/launch-context.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ReportTest {
 
-	
+	// Refer to ExampleTimeSheetReport Factory
+	// to see how a Report is Defined
+
+	// The ExampleTimesheetReportReader defines
+	// Data for the report
+
+	// refer to jobs-context.xml for Batch Job Configuration
+
+	// PDF is generated to output path defined
+	// in the batch.properties file
+
 	@Autowired
 	private JobLauncher jobLauncher;
 
 	@Autowired
 	private Job job;
-	
-	
+
 	@Test
 	public void testLaunchJob() throws Exception {
 		jobLauncher.run(job, new JobParameters());
 	}
-	
-	
-	
+
 }
